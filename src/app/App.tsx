03f7ab586dea72966e3dda01cf1b7a63d2f97fe5
Perevyazko <a.perevyazko@gmail.com>
2023-08-01 16:52:57 +0300

    Add main page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {MainPage} from "../pages/mainPage/ui/MainPage";
 
 function App() {
   return (
     <div>
-      Проект Скан
+        <MainPage/>
     </div>
   );
 }
